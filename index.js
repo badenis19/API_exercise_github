f63957d41220e856eb8bba28304e75f399b0ffc7
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-01-03 17:35:20 +0000

    API call working use async

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const fetch = require('node-fetch'); // TO RUN IN TERMINAL npm i node-fetch --save
+// fetch from https://www.youtube.com/watch?v=tVQgfKqbX3M
+// const fetch = require('node-fetch'); // TO RUN IN TERMINAL npm i node-fetch --save
+// for the JS to run after the HTML has loaded
 
+//Element from the page
+const UserInput = document.querySelector("#userid").value;
+const button = document.querySelector(".submit");
+const repoList = document.querySelector(".repoList");
+const form = document.querySelector("#form1");
+// const query = "badenis19";
 
-const input = document.getElementById("gitname");
+const getPosts = async () => {
+    const res = await fetch(`https://api.github.com/users/${UserInput}/repos`);
+    const posts = await res.json();
+    posts.forEach((post) => {
+        let content = `<li>${post.name}</li>`
+        repoList.insertAdjacentHTML("beforeend", content)
+    });
+}
 
-const test = document.getElementById("test");
-
-console.log(test)
-
-test.addEventListener("click", (e) => {
-    // console.log("clicked")
-    test.innerHTML = "NEWWW"
-})
-
-// const getPosts = () => {
-//     return fetch("https://api.github.com/users/badenis19/repos")
-//         .then(res => res.json())
-//         .then(posts => console.log(posts))
-// }
-
-// getPosts();
-
+if (form) {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault;
+        getPosts(UserInput);
+        console.log(UserInput)
+        // return false;
+    })
+}
