39b87de8873b67f49b8af0df5638604a7f86d65b
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-01-03 21:32:40 +0000

    Ajaxified search bar

--- index.js
+++ index.js
@@ -5,23 +5,51 @@
 //Element from the page
 const button = document.getElementById("button");
 const repoList = document.querySelector(".repoList");
+let userInput = document.getElementById("userid");
 
-// const getPosts = async (input) => {
-//     const res = await fetch(`https://api.github.com/users/${input}/repos`);
-//     const posts = await res.json();
-//     posts.forEach((post) => {
-//         let content = `<li>${post.name}</li>`
-//         repoList.insertAdjacentHTML("beforeend", content)
-//     });
-// }
+const getPosts = async (input) => {
+    const res = await fetch(`https://api.github.com/users/${input}/repos`);
+    const posts = await res.json();
+    if (posts.length){
+        repoList.innerHTML = ``;
+        posts.forEach((post) => {
+        let content = `<li>${post.name}</li>`
+        repoList.insertAdjacentHTML("beforeend", content)
+    });
+    } else {
+        repoList.innerHTML = ``; // clearing the previous data
+        repoList.innerHTML = `<li>Username doesn't exist</li>`;
+    }
+}
 
-
-
+// --> clicking on the button will trigger the search <---
 // if (button) {
 //     button.addEventListener("click", (e) => {
-//         const userInput = document.getElementById("userid").value;
+//         userInput = document.getElementById("userid").value;
 //         e.preventDefault;
 //         getPosts(userInput);
 //         console.log(userInput);
 //     })
 // }
+
+// ---> each key up will trigger a new search <---
+if (userInput) {
+    userInput.addEventListener("keyup", (e) => {
+        userInput = document.getElementById("userid").value;
+        e.preventDefault;
+        getPosts(userInput);
+        console.log("up");
+    })
+}
+
+// const getUsers = async () => {
+//     const res = await fetch(`https://api.github.com/users`);
+//     const users = await res.json();
+//     // console.log(users[0].site_admin)
+//     result = users.filter((user) => user.site_admin === false );
+//     console.log(result)
+// }
+
+// getUsers();
+
+// const result = words.filter(word => word.length > 6);
