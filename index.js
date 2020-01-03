2ba5a4094c576a8a1944cb15d112aecd0e145c6a
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-01-03 19:22:40 +0000

    search bar working

--- index.js
+++ index.js
@@ -3,26 +3,25 @@
 // for the JS to run after the HTML has loaded
 
 //Element from the page
-const UserInput = document.querySelector("#userid").value;
-const button = document.querySelector(".submit");
+const button = document.getElementById("button");
 const repoList = document.querySelector(".repoList");
-const form = document.querySelector("#form1");
-// const query = "badenis19";
 
-const getPosts = async () => {
-    const res = await fetch(`https://api.github.com/users/${UserInput}/repos`);
-    const posts = await res.json();
-    posts.forEach((post) => {
-        let content = `<li>${post.name}</li>`
-        repoList.insertAdjacentHTML("beforeend", content)
-    });
-}
+// const getPosts = async (input) => {
+//     const res = await fetch(`https://api.github.com/users/${input}/repos`);
+//     const posts = await res.json();
+//     posts.forEach((post) => {
+//         let content = `<li>${post.name}</li>`
+//         repoList.insertAdjacentHTML("beforeend", content)
+//     });
+// }
 
-if (form) {
-    form.addEventListener("submit", (e) => {
-        e.preventDefault;
-        getPosts(UserInput);
-        console.log(UserInput)
-        // return false;
-    })
-}
+
+
+// if (button) {
+//     button.addEventListener("click", (e) => {
+//         const userInput = document.getElementById("userid").value;
+//         e.preventDefault;
+//         getPosts(userInput);
+//         console.log(userInput);
+//     })
+// }
